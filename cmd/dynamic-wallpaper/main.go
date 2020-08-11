0ab4d24192a6d21acecc0cfcaccf22353df25f6c
bst27 <git@roesmann.net>
2020-08-12 01:55:01 +0200

    only change wallpaper once per execution

--- cmd/dynamic-wallpaper/main.go
+++ cmd/dynamic-wallpaper/main.go
@@ -1,27 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"github.com/reujab/wallpaper"
-	"time"
 )
 
 func main() {
-	background, err := wallpaper.Get()
-
+	err := changeWallpaper()
 	if err != nil {
 		panic(err)
 	}
+}
 
-	fmt.Println("Current wallpaper:", background)
-
+func changeWallpaper() error {
 	// Works fine with linux but does not revert on windows
-	err = wallpaper.SetFromURL("https://source.unsplash.com/random/1920x1080")
-	if err != nil {
-		panic(err)
-	}
-
-	time.Sleep(10 * time.Second)
-
-	wallpaper.SetFromFile(background)
+	return wallpaper.SetFromURL("https://source.unsplash.com/random/1920x1080")
 }
