7f8c81623bb5fa4919d8d6ef87b45388e1189312
bst27 <git@roesmann.net>
2020-08-12 01:35:58 +0200

    change to random wallpaper for 10 seconds and revert to previous wallpaper then

--- cmd/dynamic-wallpaper/main.go
+++ cmd/dynamic-wallpaper/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/reujab/wallpaper"
+	"time"
 )
 
 func main() {
@@ -13,4 +14,13 @@ func main() {
 	}
 
 	fmt.Println("Current wallpaper:", background)
+
+	err = wallpaper.SetFromURL("https://picsum.photos/1920/1080")
+	if err != nil {
+		panic(err)
+	}
+
+	time.Sleep(10 * time.Second)
+
+	wallpaper.SetFromFile(background)
 }
