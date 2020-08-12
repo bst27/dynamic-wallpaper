cb2c0cd14f91fbd74a4fca60a6e1fa15875aede6
bst27 <git@roesmann.net>
2020-08-12 02:45:44 +0200

    accept user input for cron expression and wallpaper url

--- cmd/dynamic-wallpaper/main.go
+++ cmd/dynamic-wallpaper/main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/reujab/wallpaper"
 	"github.com/robfig/cron/v3"
 )
 
 func main() {
+	var cronFlag = flag.String("cron", "* * * * *", "Cron expression which defines when to change wallpaper")
+	var wallpaperURL = flag.String("url", "https://source.unsplash.com/random/1920x1080", "URL which returns a wallpaper image")
+	flag.Parse()
+
 	c := cron.New()
-	c.AddFunc("* * * * *", func() {
-		_ = changeWallpaper()
+	c.AddFunc(*cronFlag, func() {
+		// Silently ignore errors and hope for success in the next try
+		_ = changeWallpaper(*wallpaperURL)
 	})
 
 	c.Start()
+
+	if len(c.Entries()) == 0 {
+		fmt.Println("Invalid cron expression. For help visit https://crontab.guru")
+		return
+	}
 
 	// Block forever until app is stopped
 	select {}
 }
 
-func changeWallpaper() error {
-	return wallpaper.SetFromURL("https://source.unsplash.com/random/1920x1080")
+func changeWallpaper(url string) error {
+	return wallpaper.SetFromURL(url)
 }
