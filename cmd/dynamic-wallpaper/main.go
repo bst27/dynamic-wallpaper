e7b8fdc4947da90a0660a314956ebaa561804044
bst27 <git@roesmann.net>
2020-08-12 02:12:18 +0200

    change wallpaper every minute

--- cmd/dynamic-wallpaper/main.go
+++ cmd/dynamic-wallpaper/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"github.com/reujab/wallpaper"
+	"github.com/robfig/cron/v3"
 )
 
 func main() {
-	err := changeWallpaper()
-	if err != nil {
-		panic(err)
-	}
+	c := cron.New()
+	c.AddFunc("* * * * *", func() {
+		_ = changeWallpaper()
+	})
+
+	c.Start()
+
+	// Block forever until app is stopped
+	select {}
 }
 
 func changeWallpaper() error {
