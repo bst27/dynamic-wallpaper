733f9fdc0831ff6f573c15c86e7b6122bec4a529
bst27 <git@roesmann.net>
2020-08-12 02:14:18 +0200

    remove outdated comment

--- cmd/dynamic-wallpaper/main.go
+++ cmd/dynamic-wallpaper/main.go
@@ -18,6 +18,5 @@ func main() {
 }
 
 func changeWallpaper() error {
-	// Works fine with linux but does not revert on windows
 	return wallpaper.SetFromURL("https://source.unsplash.com/random/1920x1080")
 }
